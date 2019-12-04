17278e604082ff4d5c5237ac8ec04c19c49f9e58
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 13:56:07 +0000

    Created ToDoTask component

---
+++ src/Enter ToDoTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ToDoTask extends React.Component {
+    render () {
+        return (
+            <p>Enter a task</p>
+        )
+    }
+}
+
+export default ToDoTask;
