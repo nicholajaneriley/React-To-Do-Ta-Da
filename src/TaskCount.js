f48180564f7ceecca8eea1f4354036a7607d0b8f
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 13:27:58 +0000

    Created TaskCount component

---
+++ src/TaskCount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TaskCount extends React.Component {
+    render() {
+        return (
+            <p>You have x tasks left to complete on your To Do list!</p>
+        );
+    }
+}
+
+
+export default TaskCount;
