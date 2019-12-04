8bb40ca6402a7510b7ddd8d064c3bd3a90db8f74
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 14:04:57 +0000

    Created TaDaTask component

---
+++ src/TaDaTask.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class TaDaTask extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-6">
+                    <p>To Do 1</p>
+                </div>
+                <div className="col-6">
+                    <div className="btn-group" role="group" aria-label="To Do List Buttons">
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-check" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-arrow-up" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TaDaTask;
