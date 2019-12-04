9f4b6fe3f8eadf6006856db9a7d73bc2bb980bf4
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 14:09:35 +0000

    Edited TaDaTask to have all buttons required

--- src/TaDaTask.js
+++ src/TaDaTask.js
@@ -4,14 +4,23 @@ class TaDaTask extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-6">
+                <div className="col-5">
                     <p>To Do 1</p>
                 </div>
-                <div className="col-6">
-                    <div className="btn-group" role="group" aria-label="To Do List Buttons">
+                <div className="col-3">
+                    <div className="btn-group" role="group" aria-label="Ta Da List Emoticon Buttons">
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-smile-o"
+                            aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-meh-o"
+                            aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-frown-o"
+                            aria-hidden="true"></i></button>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="btn-group" role="group" aria-label="Ta Da List Buttons">
                         <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-check" aria-hidden="true"></i></button>
                         <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-times" aria-hidden="true"></i></button>
-                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-arrow-up" aria-hidden="true"></i></button>
                     </div>
                 </div>
             </div>
