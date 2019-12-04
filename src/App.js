d61c0d24e322151cdb6170ec3544afdfbfb98e98
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 13:26:12 +0000

    imported Header to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         
-      </header>
+      
     </div>
   );
 }
