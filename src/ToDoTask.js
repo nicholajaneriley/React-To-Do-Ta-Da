0c1528dc0563cd5abbb7ea7445b3afef228c5b77
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 13:54:02 +0000

    Completed button group for ToDoTask

--- src/ToDoTask.js
+++ src/ToDoTask.js
@@ -10,6 +10,8 @@ class ToDoTask extends React.Component {
                 <div className="col-6">
                     <div className="btn-group" role="group" aria-label="To Do List Buttons">
                         <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-check" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-outline-info btn-sm"><i class="fa fa-arrow-up" aria-hidden="true"></i></button>
                     </div>
                 </div>
             </div>
