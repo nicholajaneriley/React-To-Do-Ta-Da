6411f8b7374f8856bf9896c9ff5499b83562a640
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-04 14:02:28 +0000

    Created InspirationalMessage component

---
+++ src/InspirationalMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InspirationalMessage extends React.Component {
+    render () {
+        return (
+            <p>This is an upbeat inspirational message about life!</p>
+        )
+    }
+}
+
+export default InspirationalMessage;
