4a6f3964c85034dfe855c95e3e3e79ffcc83923e
Nichola Riley <nicholajaneriley@gmail.com>
2019-12-15 14:22:36 +0000

    Added random message js to InspirationalMessage

--- src/InspirationalMessage.js
+++ src/InspirationalMessage.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class InspirationalMessage extends React.Component {
-    render () {
+    state = {
+        inspirations: [
+            { id: 1, inspiration: "Life is a balance of holding on and letting go - Rumi" },
+            { id: 2, inspiration: "Laughter is the fireworks of the soul - Josh Billings" },
+            { id: 3, inspiration: "To appreciate the beauty of a snowflake it is necessary to stand out in the cold - Aristotle" },
+            { id: 4, inspiration: "You can cut all the flowers, but you cannot keep spring from coming - Pablo Neruda" },
+            { id: 5, inspiration: "To fall in love with yourself is the first secret to happiness - Robert Morley" },
+            { id: 6, inspiration: "You cannot serve from an empty vessel - Eleanor Brownn" },
+        ],
+    }
+
+
+
+    render() {
+
+
+        let RandomMessage = this.state.inspirations[Math.floor(Math.random() * this.state.inspirations.length)];
+
+
         return (
-            <p>This is an upbeat inspirational message about life!</p>
+            <div className="title-message">{RandomMessage.inspiration}</div>
         )
     }
 }
