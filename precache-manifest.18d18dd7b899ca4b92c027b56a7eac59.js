889dc379af609c2bd573281b2dfb787e6d8f20e2
Nichola Riley <nicholajaneriley@gmail.com>
2020-01-06 18:36:57 +0000

    Updates

---
+++ precache-manifest.18d18dd7b899ca4b92c027b56a7eac59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a8bcaeb8ca483244dd5b22cddacf5d",
+    "url": "/React-To-Do-Ta-Da/index.html"
+  },
+  {
+    "revision": "dbadd72883fea5a779e8",
+    "url": "/React-To-Do-Ta-Da/static/css/main.55094767.chunk.css"
+  },
+  {
+    "revision": "dc42514a0907058c6e84",
+    "url": "/React-To-Do-Ta-Da/static/js/2.aaf5a274.chunk.js"
+  },
+  {
+    "revision": "dbadd72883fea5a779e8",
+    "url": "/React-To-Do-Ta-Da/static/js/main.7cd4b52a.chunk.js"
+  },
+  {
+    "revision": "bdd7e2751cf276d53ae5",
+    "url": "/React-To-Do-Ta-Da/static/js/runtime-main.cb833885.js"
+  }
+]);
